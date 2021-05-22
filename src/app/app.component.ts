a014098f2d1c2bda26f7560d30140785d1508910
kjdeepak <kjdeepak4work.com>
2021-05-23 01:41:46 +0530

    Add telephone and mail link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,19 @@ import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
-  techList = ['Angular', 'React', 'Vue', 'Flutter', 'Nodejs', 'Expressjs', 'MongoDB'];
+  techList = [
+    'Angular',
+    'React',
+    'Vue',
+    'Flutter',
+    'Nodejs',
+    'Expressjs',
+    'MongoDB',
+  ];
   faPhoneAlt = faPhoneAlt;
   faEnvelope = faEnvelope;
 }
